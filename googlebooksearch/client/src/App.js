1399fc7c7709be88fbc06a081537f20bb95cccb5
mazdamike <mwpowell44@gmail.com>
2020-08-01 17:47:58 -0400

    added navigation

--- googlebooksearch/client/src/App.js
+++ googlebooksearch/client/src/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
 import BookSearch from "./BookSearch.js"
 import SavedBooks from "./SavedBooks.js"
+import Nav from "./components/nav.js"
 
 function App() {
   return (
+    <Router>
     <div>
-      <BookSearch />
+      <Nav />
+      <Route exact path="/" component={BookSearch} />
+      <Route exact path="/saved" component={SavedBooks} />
     </div>
+    </Router>
   );
 }
-
 
 export default App;
---
+++ googlebooksearch/client/src/components/nav.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Nav() {
+    return (
+<div className="container">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark" style={{ marginBottom: 10 }}>
+      <div>
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link
+              to="/"
+              className={
+                window.location.pathname === "/" || window.location.pathname === "/search" ? "nav-link active" : "nav-link"}
+            >
+              Search
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/saved"
+              className={window.location.pathname === "/saved" ? "nav-link active" : "nav-link"}
+            >
+              Saved
+            </Link>
+          </li>
+          
+        </ul>
+      </div>
+    </nav>
+    </div>
+    );
+}
+
+export default Nav;
